2218eae0314b8cd6f9cb2a3d2dcfb79b980d62b4
Boscojohnson Arulraj <bosco.johnson@ideassion.com>
2023-07-25 18:19:47 +0530

    Updated BJ

--- Repository/AdminRepository.cs
+++ Repository/AdminRepository.cs
@@ -15,11 +15,12 @@ namespace OnboardingWebsite.Repository
         public async Task< List<DashboardVM>> GetEmployeeDetails()
         {
 
-            var deg = (from e in _context.EmployeeGeneralDetails join ee in _context.EmployeeEducationDetails on e.EmpID equals ee.Empid select ee.Passoutyear).Max();
-            var employeedetails = (from e in _context.EmployeeGeneralDetails
-                                   join l in _context.Logins on e.EmployeeName equals l.Name
-                                   join ec in _context.EmployeeContactDetails on e.EmpID equals ec.Empid
-                                   join ee in _context.EmployeeEducationDetails on e.EmpID equals ee.Empid where ee.Passoutyear == deg
+            var deg = (from e in _context.EmployeeGeneralDetails where e.Status=="A" join ee in _context.EmployeeEducationDetails on e.EmpID equals ee.Empid where ee.Status=="A" select ee.Passoutyear).Max();
+            var employeedetails = (from e in _context.EmployeeGeneralDetails where e.Status=="A"
+                                   join a in _context.Approvals on e.EmpID equals a.Empid where a.Status=="A" && a.Approved==null && a.Cancelled==null
+                                   join l in _context.Logins on e.EmployeeName equals l.Name where l.Status=="A"
+                                   join ec in _context.EmployeeContactDetails on e.EmpID equals ec.Empid where ec.Status=="A"
+                                   join ee in _context.EmployeeEducationDetails on e.EmpID equals ee.Empid where ee.Passoutyear == deg && ee.Status=="A"
                                    select new DashboardVM()
                                    {
                                        Empid = e.EmpID,
