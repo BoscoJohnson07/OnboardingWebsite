6ca794a007ac5b74e1a270f13dde6e809661b9b8
Boscojohnson Arulraj <bosco.johnson@ideassion.com>
2023-07-25 14:05:27 +0530

    Updated By Bosco Johnson 225/07/2023

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnboardingWebsite.Contracts;
+using OnboardingWebsite.Data;
+using OnboardingWebsite.Models;
+
+namespace OnboardingWebsite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminDashboardController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IAdminRepository _adminRepository;
+        public AdminDashboardController(ApplicationDbContext context,IAdminRepository adminRepository)
+        {
+            _context = context;
+            _adminRepository = adminRepository;
+        }
+
+        [HttpGet("api/AdminDashboard")]
+        public ActionResult<List<DasboardVM>> getEMployee() 
+        {
+            return _adminRepository.GetEmployeeDetails().ToList();
+        }
+
+
+    }
+}
